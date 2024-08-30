76b39215f32d44415027583bd3b1daa74aa56d5e
Vast12345 <vastprogramming@gmail.com>
2024-08-30 13:03:00 -0500

    intro to Values

--- src/main/java/com/springappdemov3/app_demo/AppDemoApplication.java
+++ src/main/java/com/springappdemov3/app_demo/AppDemoApplication.java
@@ -2,8 +2,10 @@ package com.springappdemov3.app_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:valus.properties")
 public class AppDemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/springappdemov3/app_demo/infrastructure/controller/PathVariableController.java
@@ -0,0 +1,5 @@
+package com.springappdemov3.app_demo.infrastructure.controller;
+
+public class PathVariableController {
+
+}
